5ba414efe9cae32b5b508dfe076a78b91ae89430
Bryan Chan <103339794+bchan9803@users.noreply.github.com>
2025-01-24 13:44:11 -0800

    implemented OOP and added feature to display EC map

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
-def opt1():
-  print('you chose opt 1')
+class ECSimulator():
+    def __init__(self):
+        self.ECMap = dict()
 
-def opt2():
-  print('you chose opt 2')
+    def displayMenu(self):
+        option = 0
 
-def opt3():
-  print('you chose opt 3')
+        while (option != 3):
+            print('\nPlease choose an option:')
+            print('\t1. Run simulator')
+            print('\t2. Enter custom state')
+            print('\t3. Display EC map')
+            print('\t4. Quit')
 
-print('Welcome to the Presidential College Vote Simulator!')
+            option = int(input('Enter the option: '))
+
+            if option == 1:
+                print('\nRunning simulator...')
+
+            elif option == 2:
+                newStateName = input('\nEnter new state name: ')
+                newStateECVoteCnt = int(
+                    input(f'Enter the number of electoral votes for {newStateName}: '))
+
+                self.addNewState(newStateName, newStateECVoteCnt)
+            elif option == 3:
+                self.displayECMap()
+            elif option == 4:
+                print('\nGoodbye!')
+                exit()
+            else:
+                print('\nerror! enter the correct input')
+
+    def addNewState(self, newStateName_p, newStateECVoteCnt_p):
+        self.ECMap[newStateName_p] = newStateECVoteCnt_p
+        print(f'\n{newStateName_p} has been added with {
+              newStateECVoteCnt_p} votes.')
+        self.displayMenu()
+
+    def displayECMap(self):
+        stateCount = 1
+        print('\nDisplaying EC map: ')
+        for state, stateECVoteCnt in self.ECMap.items():
+            print(f'\t{stateCount}. {state} ({stateECVoteCnt})')
+            stateCount += 1
+        self.displayMenu()
 
 
-option = 0
+def main():
+    sim = ECSimulator()
+    print('Welcome to the Presidential College Vote Simulator!')
 
-while (option != 3):
-  print('Please choose an option:')
-  print('\t 1. Run simulator')
-  print('\t 2. Enter custom state')
-  print('\t 3. Quit') 
-
-  option = int(input('Enter the option: '))
-
-  if option == 1:
-    opt1()
-  elif option == 2:
-    opt2()
-  elif option == 3:
-    opt3()
-    print('Goodbye!')
-    exit()
-  else: 
-    print('error! enter the correct input')
+    sim.displayMenu()
 
 
+if __name__ == "__main__":
+    main()
